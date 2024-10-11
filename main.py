1d6f4bae57a0a49b170ca5e08b26ef5dcb7cb1b5
Anggoro Yudho N. <70189451+anggoro-yn@users.noreply.github.com>
2024-10-11 23:54:49 +0700

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ with st.sidebar:
     st.markdown(
         """
         <div style="background-color: #a9a9a9; padding: 10px; border-radius: 5px;">
-            <p style="font-size: 16px; color: black;">Dashboard ini adalah tugas akhir Pelatihan Belajar Analisis Data dengan Python.</p>
+            <p style="font-size: 16px; color: black;">Dashboard ini adalah tugas akhir Pelatihan Belajar Analisis Data dengan Python, sebagai syarat kelulusan pelatihan.</p>
         </div>
         """,
         unsafe_allow_html=True
