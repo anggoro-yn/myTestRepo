a04df3c6317bb1e9154dd9bbfac1b9bc3bc9e9a7
Anggoro Yudho N. <70189451+anggoro-yn@users.noreply.github.com>
2024-10-12 20:00:22 +0700

    Update main.py

--- main.py
+++ main.py
@@ -261,53 +261,8 @@ else:  # TOTAL
         col1, col2 = st.columns(2)
         with col1:
             tampil_barchart_total('hr', dict_hour)
-            # Menghitung rata-rata 'cnt' per season
-            avg_cnt_per_season = hour_df.groupby('season')['cnt'].mean().reset_index()
-            
-            # Menentukan warna untuk setiap batang
-            #colors = ['gray'] * len(avg_cnt_per_season)  # Semua batang berwarna abu-abu
-            #max_index = avg_cnt_per_season['cnt'].idxmax()  # Indeks nilai maksimum
-            #min_index = avg_cnt_per_season['cnt'].idxmin()  # Indeks nilai minimum
-            
-            # Mengubah warna batang maksimum dan minimum menjadi biru
-            #colors[max_index] = 'cyan'
-            #colors[min_index] = 'cyan'
-            
-            # Membuat bar chart menggunakan seaborn
-            fig, ax = plt.subplots(figsize=(10, 6))
-            sns.barplot(ax=ax, x='season', y='cnt', data=avg_cnt_per_season) #, palette=colors)
-            
-            # Mengubah label pada sumbu x
-            season_labels = {1: 'Dingin', 2: 'Semi', 3: 'Panas', 4: 'Gugur'}
-            ax.set_xticklabels([season_labels[i] for i in avg_cnt_per_season['season']], rotation=0)
-            
-            # Menambahkan judul dan label
-            ax.set_title('Rata-rata Jumlah Pemakaian Berdasarkan Musim', fontsize=16)
-            ax.set_xlabel('Musim', fontsize=12)
-            ax.set_ylabel('Rata-rata Jumlah Pemakaian', fontsize=12)
-            
-            # Mengatur batas maksimum sumbu y
-            ax.set_ylim(0, 300)
-            
-            # Menampilkan grafik di Streamlit
-            st.pyplot(fig)
         with col2:
-            # Membuat figure untuk boxplot
-            fig, ax = plt.subplots(figsize=(10, 6))
-            
-            # Membuat boxplot dengan hue berdasarkan tahun (yr)
-            sns.boxplot(x='season', y='cnt', data=hour_df, ax=ax)
-            
-            # Menambahkan judul dan label pada grafik
-            ax.set_title('Penggunaan Sepeda per Musim', fontsize=16)
-            ax.set_xlabel('Musim', fontsize=12)
-            ax.set_ylabel('Jumlah Penggunaan Sepeda', fontsize=12)
-            
-            # Mengubah label pada sumbu x untuk musim
-            ax.set_xticklabels(['Dingin', 'Semi', 'Panas', 'Gugur'])
-            
-            # Menampilkan grafik di Streamlit
-            st.pyplot(fig)
+            tampil_boxplot_total('hr', list_hour)
     with tabHari:
         col1, col2 = st.columns(2)
         with col1:
