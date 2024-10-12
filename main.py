b88d46b6ad89d415b4431a79d7819ab9a0da8237
Anggoro Yudho N. <70189451+anggoro-yn@users.noreply.github.com>
2024-10-12 18:45:07 +0700

    Update main.py

--- main.py
+++ main.py
@@ -255,7 +255,7 @@ if option_2_1 == 'Per tahun':
         with col1:
             tampil_barchart_pertahun('weekday', dict_week)
         with col2:
-            tampil_boxplot_pertahun('weekday', list_week)
+            tampil_boxplot_pertahun('weekday', ['Min', 'Sen', 'Sel', 'Rab', 'Kam', 'Jum', 'Sab'])
             # Membuat figure untuk boxplot
             fig, ax = plt.subplots(figsize=(10, 6))
             
