27bc9733c4bc3dcd7647edb663fbfb3ce5a69c13
Anggoro Yudho N. <70189451+anggoro-yn@users.noreply.github.com>
2024-10-15 20:28:46 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 import plotly.express as px
 import matplotlib.pyplot as plt
 
+# Fungsi untuk membuat grafik pemakaian listrik dalam sepuluh hari terakhir
 def buat_grafik_kwh(column_name, nilai_rata2, judul):
     # Membuat grafik batang
     fig, ax = plt.subplots(figsize=(10, 6))
@@ -306,68 +307,8 @@ if admin_user or general_user:
             
             # Menampilkan grafik di Streamlit
             st.pyplot(fig)
+            buat_grafik_kwh('SUM ALL APF Area', nilai_Sum_APF_rata2, 'Jumlah Pemakaian Listrik Seluruh Plant dalam 10 hari terakhir')
         with tab4:
-            # Membuat grafik batang
-            fig, ax = plt.subplots(figsize=(10, 6))
-
-
-            # Menentukan nilai maksimum, minimum, dan tanggal terbaru
-            max_value = df_10_hari['EMS'].max()
-            min_value = df_10_hari['EMS'].min()
-            latest_date = df_10_hari['Tanggal'].max()
-            
-            colors = []
-            for index, row in df_10_hari.iterrows():
-                if row['EMS'] == max_value:
-                    colors.append('crimson')  # Warna untuk nilai y maksimal
-                elif row['EMS'] == min_value:
-                    colors.append('limegreen')  # Warna untuk nilai y minimal
-                elif row['Tanggal'] == latest_date:
-                    colors.append('turquoise')  # Warna untuk tanggal terbaru
-                else:
-                    colors.append('lightgray')  # Warna untuk batang lainnya            
-
-            bars = ax.bar(df_10_hari['Tanggal'], df_10_hari['EMS'], color=colors)
-
-            # Menambahkan garis horizontal putus-putus
-            ax.axhline(y=nilai_ems_rata2, color='black', linestyle='--') 
-
-            # Mengatur warna batang untuk tanggal terakhir
-            #bars[0].set_color('cyan')
-            
-            # Mengatur posisi tick label di tengah batang
-            ax.set_xticks(df_10_hari['Tanggal'])
-            ax.set_xticklabels(df_10_hari['Tanggal'].dt.strftime('%Y-%m-%d'), rotation=90, ha='center')
-            
-            # Menghitung batas sumbu y
-            min_value = df_10_hari['EMS'].min()
-            max_value = df_10_hari['EMS'].max()
-            y_min = min_value - 0.025 * min_value
-            y_max = max_value + 0.01 * max_value
-
-            # Mengatur batas sumbu y
-            ax.set_ylim(y_min, y_max)
-
-            # Menambahkan label dan judul
-            plt.xlabel('Tanggal')
-            plt.ylabel('PLN')
-            plt.title('Konsumsi Listrik Sesuai EMS dalam 10 Hari Terakhir')
-            
-            # Menampilkan grafik di Streamlit
-            st.pyplot(fig)
-
-            # Memberikan catatan
-            st.write("Note:Y-axis doesn't start from zero to emphasize fluctuation.")
-            # Tulisan "Highest", "Lowest", dan "Latest" dalam satu baris dengan warna berbeda
-            st.markdown("""
-            <p>
-                <span style="color:crimson;">Highest </span> 
-                <span style="color:limegreen;"> Lowest </span> 
-                <span style="color:turquoise;"> Latest </span>
-                <span style="color:black;"> - - - (average)</span> 
-            </p>
-            """, unsafe_allow_html=True)
-            
             buat_grafik_kwh('EMS', nilai_ems_rata2, 'Konsumsi listrik berdasar EMS dalam 10 hari terakhir')
 
 
