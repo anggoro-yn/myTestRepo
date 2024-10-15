26db16f5dabc5526cf26d688cde6db38ba2f7350
Anggoro Yudho N. <70189451+anggoro-yn@users.noreply.github.com>
2024-10-15 14:24:20 +0700

    Update main.py

--- main.py
+++ main.py
@@ -75,12 +75,11 @@ with st.sidebar:
         admin_user = False
         general_user = False
         
-#    if st.button('Change Password'):
-#        password_baru = st.text_input("New password", type='password')
-#        dict_user[name] = password_baru
+    if st.button('Change Password'):
+        password_baru = st.text_input("New password", type='password')
+        dict_user[name] = password_baru
 
-#    st.write(dict_user)
-#    st.write(dict_name)
+    st.write(dict_user)
     
 
 if admin_user or general_user:
